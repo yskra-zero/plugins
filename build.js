1c520a8aaa79c4c84391762dcc23a52f6a53be40
yskra-zero <227346828+yskra-zero@users.noreply.github.com>
2025-08-22 19:51:19 +0700

    fix build ?

--- build.js
+++ build.js
@@ -2,22 +2,28 @@
 
 import { exec } from 'node:child_process';
 import { copyFile } from 'node:fs';
+import { promisify } from 'node:util';
 import { glob } from 'glob';
 
-glob('**/**/manifest.json')
-  .then((files) => files
+const execAsync = promisify(exec);
+
+// eslint-disable-next-line antfu/no-top-level-await
+await glob('**/**/manifest.json')
+  .then((files) => Promise.all(files
     .map((file) => file.replace('manifest.json', ''))
-    .map((target) => exec(`TARGET=${target} pnpx vite build`, (err, stdout, stderr) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
-    })),
+    .map((target) =>
+      execAsync(`TARGET=${target} pnpx vite build`)
+        .then(({ stdout, stderr }) => {
+          console.log(`stdout: ${stdout}`);
+          console.error(`stderr: ${stderr}`);
+        })
+        .catch((err) => {
+          console.error(err);
+        }),
+    )),
   );
 
-copyFile('./repo.json', 'dist/repo.json', (err) => {
+copyFile('repo.json', 'dist/repo.json', (err) => {
   if (err) {
     throw err;
   }
