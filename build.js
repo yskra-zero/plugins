2f80819d984f6fa6cc59d8e6520786431d5d1f03
yskra-zero <227346828+yskra-zero@users.noreply.github.com>
2025-08-22 20:10:19 +0700

    index file

--- build.js
+++ build.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-console */
 
 import { exec } from 'node:child_process';
-import { copyFile } from 'node:fs';
+import { copyFile, readdir, writeFile } from 'node:fs';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { promisify } from 'node:util';
 import { glob } from 'glob';
 
 const execAsync = promisify(exec);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // eslint-disable-next-line antfu/no-top-level-await
 await glob('**/**/manifest.json')
@@ -28,4 +31,40 @@ copyFile('repo.json', 'dist/repo.json', (err) => {
     throw err;
   }
   console.log('dist/repo.json');
+});
+
+readdir(resolve(__dirname, 'dist'), (err, files) => {
+  if (err) {
+    console.error('Ошибка при чтении папки dist:', err);
+    return;
+  }
+
+  const links = files
+    .map((f) => `<li><a href="./${f}">${f}</a></li>`)
+    .join('\n');
+
+  const html = `
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8" />
+  <title>Yskra plugins</title>
+</head>
+<body>
+  <h1>Yskra plugins</h1>
+  <ul>
+    ${links}
+  </ul>
+</body>
+</html>
+`;
+
+  writeFile('dist/index.html', html, (writeErr) => {
+    if (writeErr) {
+      console.error(writeErr);
+    }
+    else {
+      console.log('dist/index.html');
+    }
+  });
 });
